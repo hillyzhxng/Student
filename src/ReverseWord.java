36cfc60437ba29ce583b93aaf24b145f909b7424
Hillary <zhanghi@sheridancollege.ca>
2023-02-01 16:51:19 -0500

    added comment for pull

--- src/ReverseWord.java
+++ src/ReverseWord.java
@@ -3,6 +3,7 @@ public class ReverseWord
     public static void main (String[] args)
     {
         //added comment for fetch+merge 
+        //added comment for pull
         
         char[] myArray = new char [7];
         myArray [0] = 's';
